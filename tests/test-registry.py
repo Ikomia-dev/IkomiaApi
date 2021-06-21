bc6da6db9f63ae55fb48d516f94d79502db1c51a
Ludo <ludovic.barusseau@gmail.com>
2021-06-21 20:35:54 +0200

    Rename store.py -> registry.py

--- tests/test-registry.py
+++ tests/test-registry.py
@@ -1,7 +1,7 @@
 import os
 import logging
 import ikomia
-from ikomia.dataprocess import store
+from ikomia.dataprocess import registry
 import cv2
 
 logger = logging.getLogger(__name__)
@@ -18,15 +18,15 @@ def get_test_video_directory():
 
 
 def test_get_local_algorithms():
-    registry = store.IkomiaRegistry()
-    algos = registry.getAlgorithms()
+    reg = registry.IkomiaRegistry()
+    algos = reg.getAlgorithms()
     logger.info("Number of algorithms:" + str(len(algos)))
     logger.info(algos)
 
 
 def test_get_online_algorithms():
-    registry = store.IkomiaRegistry()
-    algos = registry.get_online_algorithms()
+    reg = registry.IkomiaRegistry()
+    algos = reg.get_online_algorithms()
     logger.info("Number of online algorithms:" + str(len(algos)))
 
     for algo in algos:
@@ -34,26 +34,26 @@ def test_get_online_algorithms():
 
 
 def test_download_plugin():
-    registry = store.IkomiaRegistry()
+    reg = registry.IkomiaRegistry()
     logger.info("Downloading RAFTOpticalFlow...")
-    registry._download_plugin("RAFTOpticalFlow")
+    reg._download_plugin("RAFTOpticalFlow")
 
 
 def test_install_plugin():
-    registry = store.IkomiaRegistry()
+    reg = registry.IkomiaRegistry()
     logger.info("Installing RAFTOpticalFlow...")
-    registry._install_plugin("RAFTOpticalFlow")
+    reg._install_plugin("RAFTOpticalFlow")
     logger.info("Installing MobileNet SSD...")
-    registry._install_plugin("MobileNet SSD")
+    reg._install_plugin("MobileNet SSD")
 
 
 def test_local_instantiation():
-    registry = store.IkomiaRegistry()
-    algo_names = registry.getAlgorithms()
+    reg = registry.IkomiaRegistry()
+    algo_names = reg.getAlgorithms()
     algos = []
 
     for name in algo_names:
-        algo = registry.createInstance(name)
+        algo = reg.createInstance(name)
         logger.info(algo.name)
         algos.append(algo)
 
@@ -65,45 +65,45 @@ def test_local_instantiation():
 
 
 def test_instantiation():
-    registry = store.IkomiaRegistry()
+    reg = registry.IkomiaRegistry()
     # Local C++ algo
     logger.info("Instantiate CLAHE algorithm...")
-    algo = registry.create_algorithm("CLAHE")
+    algo = reg.create_algorithm("CLAHE")
     assert(algo is not None)
     logger.info("Done.")
     # Local Python algo
     logger.info("Instantiate ResNet Train algorithm...")
-    algo = registry.create_algorithm("ResNet Train")
+    algo = reg.create_algorithm("ResNet Train")
     assert (algo is not None)
     logger.info("Done.")
     # Online C++ algo
     logger.info("Instantiate MobileNet SSD algorithm...")
-    algo = registry.create_algorithm("MobileNet SSD")
+    algo = reg.create_algorithm("MobileNet SSD")
     logger.info("Done.")
     # Online Python algo
     logger.info("Instantiate RAFTOpticalFlow algorithm...")
-    algo = registry.create_algorithm("RAFTOpticalFlow")
+    algo = reg.create_algorithm("RAFTOpticalFlow")
     logger.info("Done.")
 
 
 def test_update():
-    registry = store.IkomiaRegistry()
+    reg = registry.IkomiaRegistry()
     logger.info("Updating ResNet Train algorithm...")
-    registry.update_algorithm("ResNet Train")
+    reg.update_algorithm("ResNet Train")
     logger.info("Updating all algorithms...")
-    registry.update_algorithms()
+    reg.update_algorithms()
 
 
 def test_execution():
-    registry = store.IkomiaRegistry()
+    reg = registry.IkomiaRegistry()
 
     # Load image
     img_path = get_test_image_directory() + "/Lena.png"
     img = cv2.imread(img_path)
     cv2.imshow("Original", img)
 
-    # OpenCV CLAHE
-    algo = registry.create_algorithm("CLAHE")
+    # OpenCV CLAHE (C++)
+    algo = reg.create_algorithm("CLAHE")
     input_img = algo.getInput(0)
     input_img.setImage(img)
     algo.run()
@@ -111,8 +111,8 @@ def test_execution():
     img_out = output.getImage()
     cv2.imshow("CLAHE", img_out)
 
-    # Marketplace FaceDetector
-    algo = registry.create_algorithm("Face Detector")
+    # Marketplace FaceDetector (C++)
+    algo = reg.create_algorithm("Face Detector")
     input_img = algo.getInput(0)
     input_img.setImage(img)
     algo.run()
@@ -121,16 +121,25 @@ def test_execution():
     output_img.drawGraphics(output_graphics)
     img_out = output_img.getImage()
     cv2.imshow("Face Detector", img_out)
+
+    # Marketplace scikit_threshold (Python)
+    algo = reg.create_algorithm("scikit_threshold")
+    input_img = algo.getInput(0)
+    input_img.setImage(img)
+    algo.run()
+    output = algo.getOutput(0)
+    img_out = output.getImage()
+    cv2.imshow("scikit_threshold", img_out)
     cv2.waitKey(0)
 
 
 if __name__ == "__main__":
     ikomia.initialize("Ludo", "ludo?imageez")
-    # test_get_local_algorithms()
+    test_get_local_algorithms()
     # test_get_online_algorithms()
     # test_download_plugin()
     # test_install_plugin()
-    # test_local_instantiation()
+    test_local_instantiation()
     # test_instantiation()
     # test_update()
-    test_execution()
+    # test_execution()
