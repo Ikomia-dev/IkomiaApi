b8ce225dd78996897e9bd9a6ff6c52c104b317b0
Ludo <ludovic.barusseau@gmail.com>
2021-06-23 17:24:03 +0200

    Add script to start mlflow and tensorboard

---
+++ scripts/start_train_monitoring.py
@@ -0,0 +1,25 @@
+import subprocess
+from ikomia.core import config
+
+
+def start_mlflow():
+    proc = subprocess.Popen(["mlflow", "server",
+                             "--backend-store-uri", config.main_cfg["mlflow"]["store_uri"],
+                             "-- default-artifact-root", config.main_cfg["mlflow"]["artifact_uri"],
+                             "--host", "0.0.0.0"])
+    poll = proc.poll()
+    if poll is None:
+        print("MLflow server started successfully at ", config.main_cfg["mlflow"]["tracking_uri"])
+
+
+def start_tensorboard():
+    proc = subprocess.Popen(["tensorboard",
+                             "--logdir", config.main_cfg["tensorboard"]["log_uri"]])
+    poll = proc.poll()
+    if poll is None:
+        print("Tensorboard server started successfully at ", config.main_cfg["tensorboard"]["tracking_uri"])
+
+
+if __name__ == "__main__":
+    start_mlflow()
+    start_tensorboard()
