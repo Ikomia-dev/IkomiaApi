30ed74decea29085ffa138885421934c50c59f2f
Ludo <ludovic.barusseau@gmail.com>
2021-07-13 16:31:16 +0200

    Update parameters change test

--- tests/test-task.py
+++ tests/test-task.py
@@ -2,7 +2,7 @@ import ikomia
 import logging
 import cv2
 from ikomia.utils import tests
-from ikomia.core import task
+from ikomia.core import task, ParamMap
 from ikomia.dataprocess import registry, displayIO
 
 logger = logging.getLogger(__name__)
@@ -17,15 +17,30 @@ def test_task_parameters(ik_registry):
     input_img = algo.getInput(0)
     input_img.setImage(img)
     algo.run()
-    displayIO.display(algo, algo.name)
+    displayIO.display(algo.getOutput(0), algo.name)
 
-    # change parameters value
-    logger.info("----- Run with manually set parameters -----")
+    # change parameters value from core.CParamMap
+    logger.info("----- Run with manually set parameters (CParamMap)-----")
+    params = ParamMap()
+    params["borderType"] = str(4)
+    params["anchorX"] = str(-1)
+    params["anchorY"] = str(-1)
+    params["kSizeHeight"] = str(13)
+    params["kSizeWidth"] = str(13)
+    params["bNormalize"] = str(1)
+    params["ddepth"] = str(-1)
+    algo.getParam().setParamMap(params)
+    logger.info(algo.getParam())
+    algo.run()
+    displayIO.display(algo.getOutput(0), algo.name)
+
+    # change parameters value from dict
+    logger.info("----- Run with manually set parameters (dict)-----")
     task.set_parameter(algo, {"borderType": 4, "anchorX": -1, "anchorY": -1, "kSizeHeight": 29, "kSizeWidth": 29,
                               "bNormalize": 1, "ddepth": -1})
     logger.info(algo.getParam())
     algo.run()
-    displayIO.display(algo, algo.name)
+    displayIO.display(algo.getOutput(0), algo.name)
 
 
 if __name__ == "__main__":
