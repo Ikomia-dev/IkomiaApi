b0d9688474ff4662a960eb4a403b56ebd1b44bc0
Ludo <ludovic.barusseau@gmail.com>
2021-08-27 09:04:12 +0200

    Add methods to get common outputs from their types

--- ikomia/core/task.py
+++ ikomia/core/task.py
@@ -35,7 +35,7 @@ class TaskParam(core.CWorkflowTaskParam):
         raise NotImplementedError
 
 
-def set_parameter(task, params: dict):
+def set_parameters(task, params: dict):
     """
     Set parameters of the given task from a dict structure.
 
@@ -46,10 +46,64 @@ def set_parameter(task, params: dict):
     if task is None:
         return
 
-    param_map = core.ParamMap()
-
+    param_map = task.getParamValues()
     for key in params:
         param_map[str(key)] = str(params[key])
 
     param = task.getParam()
     param.setParamMap(param_map)
+
+
+def _get_outputs(task, types, index=1):
+    if task is None:
+        raise RuntimeError("Cannot get outputs from None task.")
+
+    outputs = []
+    all_outputs = task.getOutputs()
+
+    for output in all_outputs:
+        if output.dataType in types:
+            outputs.append(output)
+
+    if 0 <= index < len(outputs):
+        return outputs[index]
+    elif len(outputs) == 1:
+        return outputs[0]
+    else:
+        return outputs
+
+
+def get_image_output(task, index=-1):
+    # data_types = [core.IODataType.IMAGE, core.IODataType.IMAGE_BINARY, core.IODataType.IMAGE_LABEL]
+    data_types = [0, 1, 2]
+    return _get_outputs(task, data_types, index)
+
+
+def get_graphics_output(task, index=-1):
+    data_types = [core.IODataType.OUTPUT_GRAPHICS]
+    return _get_outputs(task, data_types, index)
+
+
+def get_numeric_output(task, index=-1):
+    data_types = [core.IODataType.NUMERIC_VALUES]
+    return _get_outputs(task, data_types, index)
+
+
+def get_blob_measure_output(task, index=-1):
+    data_types = [core.IODataType.BLOB_VALUES]
+    return _get_outputs(task, data_types, index)
+
+
+def get_dataset_output(task, index=-1):
+    data_types = [core.IODataType.DNN_DATASET]
+    return _get_outputs(task, data_types, index)
+
+
+def get_array_output(task, index=-1):
+    data_types = [core.IODataType.ARRAY]
+    return _get_outputs(task, data_types, index)
+
+
+def get_path_output(task, index=-1):
+    data_types = [core.IODataType.FOLDER_PATH, core.IODataType.FILE_PATH]
+    return _get_outputs(task, data_types, index)
--- tests/test-task.py
+++ tests/test-task.py
@@ -3,7 +3,7 @@ import logging
 import cv2
 from ikomia.utils import tests
 from ikomia.core import task, ParamMap
-from ikomia.dataprocess import displayIO
+from ikomia.dataprocess import workflow, displayIO
 
 logger = logging.getLogger(__name__)
 
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 def test_task_parameters():
     logger.info("===== Test::set task parameters =====")
     algo = ikomia.ik_registry.create_algorithm("Box Filter")
-    logger.info("----- Run with default parameters")
+    logger.info("----- Use default parameters")
     logger.info(algo.getParam())
     img_path = tests.get_test_image_directory() + "/Lena.png"
     img = cv2.imread(img_path)
@@ -20,8 +20,7 @@ def test_task_parameters():
     algo.run()
     displayIO.display(algo.getOutput(0), algo.name)
 
-    # change parameters value from core.CParamMap
-    logger.info("----- Run with manually set parameters (CParamMap)")
+    logger.info("----- Manually set all parameters from ParamMap")
     params = ParamMap()
     params["borderType"] = str(4)
     params["anchorX"] = str(-1)
@@ -30,19 +29,52 @@ def test_task_parameters():
     params["kSizeWidth"] = str(13)
     params["bNormalize"] = str(1)
     params["ddepth"] = str(-1)
-    algo.getParam().setParamMap(params)
+    algo.setParamValues(params)
     logger.info(algo.getParam())
     algo.run()
     displayIO.display(algo.getOutput(0), algo.name)
 
-    # change parameters value from dict
-    logger.info("----- Run with manually set parameters (dict)")
-    task.set_parameter(algo, {"borderType": 4, "anchorX": -1, "anchorY": -1, "kSizeHeight": 29, "kSizeWidth": 29,
+    logger.info("----- Manually set some of the parameters from ParamMap")
+    params = algo.getParamValues()
+    params["kSizeHeight"] = str(31)
+    params["kSizeWidth"] = str(31)
+    algo.setParamValues(params)
+    logger.info(algo.getParam())
+    algo.run()
+    displayIO.display(algo.getOutput(0), algo.name)
+
+    logger.info("----- Manually set all parameters from dict")
+    task.set_parameters(algo, {"borderType": 4, "anchorX": -1, "anchorY": -1, "kSizeHeight": 29, "kSizeWidth": 29,
                               "bNormalize": 1, "ddepth": -1})
     logger.info(algo.getParam())
     algo.run()
     displayIO.display(algo.getOutput(0), algo.name)
 
+    logger.info("----- Manually set some parameters from dict")
+    task.set_parameters(algo, {"kSizeHeight": 29, "kSizeWidth": 29})
+    logger.info(algo.getParam())
+    algo.run()
+    displayIO.display(algo.getOutput(0), algo.name)
+
+
+def test_get_task_outputs():
+    logger.info("===== Test::get task outputs of various data types =====")
+    img_path = tests.get_test_image_directory() + "/Lena.png"
+    wf_path = tests.get_test_workflow_directory() + "/WorkflowTestOutput.json"
+    wf = workflow.Workflow("test_single_image_run", ikomia.ik_registry)
+    wf.load(wf_path)
+    wf.set_image_input(path=img_path)
+    wf.run()
+
+    detector_task = wf.find_task("MobileNet SSD", 0)[1]
+    img_out = task.get_image_output(detector_task)
+    displayIO.display(img_out, detector_task.name)
+    graphics_out = task.get_graphics_output(detector_task)
+    displayIO.display(graphics_out, detector_task.name)
+    blob_out = task.get_blob_measure_output(detector_task)
+    displayIO.display(blob_out, detector_task.name)
+
 
 if __name__ == "__main__":
     test_task_parameters()
+    test_get_task_outputs()
