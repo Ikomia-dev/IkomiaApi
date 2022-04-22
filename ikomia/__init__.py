ec9d85053f68892fc325cfd5c63c8157e5ed14a7
Ludo <ludovic.barusseau@gmail.com>
2022-04-22 17:20:36 +0200

    Improve file logging

--- ikomia/__init__.py
+++ ikomia/__init__.py
@@ -14,12 +14,12 @@
 
 import os
 import sys
-import logging
 from ikomia import utils
 from ikomia.utils import autocomplete
 from ikomia.core.auth import LoginSession
 from ikomia.core import config
 from ikomia.dataprocess import registry
+
 
 # Add built-in OpenCV to Python path
 if not utils.isAppStarted():
@@ -30,13 +30,12 @@ if not utils.isAppStarted():
     else:
         sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + os.sep + "opencv")
 
+
 global ik_api_session
 ik_api_session = None
 
 global ik_registry
 ik_registry = None
-
-logger = logging.getLogger()
 
 
 def authenticate():
@@ -50,51 +49,6 @@ def authenticate():
     if config.main_cfg["registry"]["auto_completion"]:
         autocomplete.make_online_plugins()
 
-
-def _check_directories():
-    os.makedirs(config.main_cfg["root_folder"], exist_ok=True)
-    os.makedirs(config.main_cfg["registry"]["path"], exist_ok=True)
-    os.makedirs(os.path.join(config.main_cfg["registry"]["path"], "C++/"), exist_ok=True)
-    os.makedirs(os.path.join(config.main_cfg["registry"]["path"], "Python/"), exist_ok=True)
-    os.makedirs(os.path.join(config.main_cfg["registry"]["path"], "Transfer/"), exist_ok=True)
-    os.makedirs(config.main_cfg["workflow"]["path"], exist_ok=True)
-    os.makedirs(config.main_cfg["data"]["path"], exist_ok=True)
-    os.makedirs(config.main_cfg["mlflow"]["artifact_uri"], exist_ok=True)
-    os.makedirs(config.main_cfg["mlflow"]["store_uri"], exist_ok=True)
-    os.makedirs(config.main_cfg["tensorboard"]["log_uri"], exist_ok=True)
-
-
-def init_logging(rank=-1):
-    if rank in [-1, 0]:
-        logger.handlers = []
-        logger.setLevel(logging.INFO)
-
-        # log to stdout and stderr
-        formatter = logging.Formatter("%(message)s")
-        info = logging.StreamHandler(sys.stdout)
-        info.setLevel(logging.INFO)
-        info.setFormatter(formatter)
-        logger.addHandler(info)
-
-        err = logging.StreamHandler(sys.stderr)
-        err.setLevel(logging.ERROR)
-        err.setFormatter(formatter)
-        logger.addHandler(err)
-
-        # log to file
-        log_path = config.main_cfg["root_folder"] + "/log.txt"
-        file_formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s", "%Y-%m-%d %H:%M:%S")
-        file_handler = logging.FileHandler(log_path, 'w')
-        file_handler.setLevel(logging.DEBUG)
-        file_handler.setFormatter(file_formatter)
-        logger.addHandler(file_handler)
-    else:
-        logging.basicConfig(format="%(message)s", level=logging.WARN)
-
-
-# API initialization
-_check_directories()
-init_logging()
 
 if not utils.isAppStarted():
     ik_registry = registry.IkomiaRegistry()
--- ikomia/core/__init__.py
+++ ikomia/core/__init__.py
@@ -12,6 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+import sys
+import logging
+import time
+
 try:
     # Valid for Ikomia Studio or Linux platform
     from ikomia.core.pycore import *
@@ -22,5 +27,57 @@ except:
 from ikomia.core import config
 
 
+logger = logging.getLogger()
+
+
+def _check_directories():
+    os.makedirs(config.main_cfg["root_folder"], exist_ok=True)
+    os.makedirs(config.main_cfg["registry"]["path"], exist_ok=True)
+    os.makedirs(os.path.join(config.main_cfg["registry"]["path"], "C++/"), exist_ok=True)
+    os.makedirs(os.path.join(config.main_cfg["registry"]["path"], "Python/"), exist_ok=True)
+    os.makedirs(os.path.join(config.main_cfg["registry"]["path"], "Transfer/"), exist_ok=True)
+    os.makedirs(config.main_cfg["workflow"]["path"], exist_ok=True)
+    os.makedirs(config.main_cfg["data"]["path"], exist_ok=True)
+    os.makedirs(config.main_cfg["mlflow"]["artifact_uri"], exist_ok=True)
+    os.makedirs(config.main_cfg["mlflow"]["store_uri"], exist_ok=True)
+    os.makedirs(config.main_cfg["tensorboard"]["log_uri"], exist_ok=True)
+
+
+def _init_logging(rank=-1):
+    if rank in [-1, 0]:
+        logger.handlers = []
+        logger.setLevel(logging.INFO)
+
+        # log to stdout and stderr
+        formatter = logging.Formatter("%(message)s")
+        info = logging.StreamHandler(sys.stdout)
+        info.setLevel(logging.INFO)
+        info.setFormatter(formatter)
+        logger.addHandler(info)
+
+        err = logging.StreamHandler(sys.stderr)
+        err.setLevel(logging.ERROR)
+        err.setFormatter(formatter)
+        logger.addHandler(err)
+
+        # log to file
+        log_path = config.main_cfg["root_folder"] + "/log.txt"
+        file_formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s", "%Y-%m-%d %H:%M:%S")
+        file_handler = logging.FileHandler(log_path, 'a')
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(file_formatter)
+        logger.addHandler(file_handler)
+
+        with open(log_path, "w") as f:
+            logger.info(f"Logging started at {time.strftime('%Y-%m-%d %H:%M:%S')}")
+    else:
+        logging.basicConfig(format="%(message)s", level=logging.WARN)
+
+
 def get_ikomia_root_folder():
     return config.main_cfg["root_folder"]
+
+
+# API initialization
+_check_directories()
+_init_logging()
