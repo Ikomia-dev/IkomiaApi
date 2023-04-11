e2396523b3dbceb5df552d16096bf2cc44951363
yom <guillaume.demarcq@ikomia.com>
2023-04-11 10:10:31 +0200

    - Small update

--- doc/make_html.sh
+++ doc/make_html.sh
@@ -1,3 +1,3 @@
 LD_LIBRARY_PATH="/usr/local/lib:$(pwd)/../../IkomiaCore/Build/lib"
 export LD_LIBRARY_PATH
-#sphinx-autobuild source build/html --host 192.168.19.16
+sphinx-autobuild source build/html
