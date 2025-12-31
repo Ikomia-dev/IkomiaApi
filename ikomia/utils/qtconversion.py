7fab987019790672263b8622e6010c50867d9f23
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2025-12-31 10:50:38 +0100

    chore: replace PySide2 by PySide6

--- ikomia/utils/qtconversion.py
+++ ikomia/utils/qtconversion.py
@@ -33,13 +33,13 @@ def PyQtToQt(pyqt_widget):
 
 def PySideToQt(pyside_widget):
     """
-    Wraps PySide2 widget instance to C++ Qt
+    Wraps PySide6 widget instance to C++ Qt
 
     Args:
-        pyqt_widget : instance of the PyQt object
+        pyside_widget : instance of the PySide object
 
     Returns:
         QWidget*: C++ wrapper instance
     """
-    import shiboken2
-    return shiboken2.getCppPointer(pyside_widget)[0]
+    import shiboken6
+    return shiboken6.getCppPointer(pyside_widget)[0]
