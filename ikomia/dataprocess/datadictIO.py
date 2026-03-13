68f5c806d1d63a4387b9e41db1dc23583527f42a
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2026-03-13 15:13:09 +0100

    fix: add shim module to ensure retro-compatibility

---
+++ ikomia/dataprocess/datadictIO.py
@@ -0,0 +1,9 @@
+import warnings
+
+from ikomia.dataprocess.io.datadictIO import *
+
+warnings.warn(
+    "ikomia.dataprocess.datadictIO is deprecated, use ikomia.dataprocess.io.datadictIO",
+    FutureWarning,
+    stacklevel=2
+)
