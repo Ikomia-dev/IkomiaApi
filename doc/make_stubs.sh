d4e4d7cbb4bc2f939a7583dcd511fdbf5e43c7f6
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2026-05-12 13:49:47 +0200

    chore: use env variable to set python command

--- doc/make_stubs.sh
+++ doc/make_stubs.sh
@@ -1,7 +1,8 @@
 LD_LIBRARY_PATH="/usr/local/lib:$(pwd)/../../IkomiaCore/Build/lib:$(pwd)/../../gmic/build:$(pwd)/../../qwt/build/lib"
 export LD_LIBRARY_PATH
+PYTHON=${PYTHON:-python}
 # Generate stubs for C++ libs
-python gen_stubs.py -m ikomia.core.pycore -o $(pwd)/../
-python gen_stubs.py -m ikomia.dataio.pydataio -o $(pwd)/../
-python gen_stubs.py -m ikomia.dataprocess.pydataprocess -o $(pwd)/../
-python gen_stubs.py -m ikomia.utils.pyutils -o $(pwd)/../
+$PYTHON gen_stubs.py -m ikomia.core.pycore -o $(pwd)/../
+$PYTHON gen_stubs.py -m ikomia.dataio.pydataio -o $(pwd)/../
+$PYTHON gen_stubs.py -m ikomia.dataprocess.pydataprocess -o $(pwd)/../
+$PYTHON gen_stubs.py -m ikomia.utils.pyutils -o $(pwd)/../
